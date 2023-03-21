dc05f8d1db2be4cc2e537a84af4b050e02affc3b
Jett Langhorn <jlanghor@stevens.edu>
2023-03-20 21:53:32 -0400

    Add positive signal reconstruction using IFFT

--- DataProcessing.m
+++ DataProcessing.m
@@ -1,6 +1,6 @@
 clear all;clc;close all;
 
-timeconst = 0.2;    %Time constant for the PCB Transducer in s
+timeconst = 0.2;    %Time constant for the PCB Transducer in seconds
 conv = 4.957;       %Conversion Factor in V/Psi
 Pamb = 14.7;        %Ambient Pressure for the Test
 LBand = 50;       %Lower frequency limit
@@ -37,11 +37,11 @@ ylabel("|P1(f)|")
 %Remove afterwards
 LBand = round(LBand*L/Fs)+1;
 HBand = round(HBand*L/Fs)+1;
-Neg = Y;
-Neg(1,HBand) = complex(0,0);
-Neg(1,LBand) = complex(0,0);
-Neg(1,HBand+round(L/2)) = complex(0,0);
-Neg(1,L-LBand) = complex(0,0);
+Pos = complex(zeros(1,1501),0);
+Pos(1,HBand) = Y(1,HBand);
+Pos(1,LBand) = Y(1,LBand);
+Pos(1,L-HBand) = Y(1,HBand);
+Pos(1,L-LBand) = Y(1,LBand);
 
 %LBand = ceil(LBand*L/Fs)+1;
 %HBand = floor(HBand*L/Fs)+1;
@@ -51,16 +51,25 @@ Neg(1,L-LBand) = complex(0,0);
 %Neg(1,HBand:(L/2)+1) = Y(1,HBand:(L/2)+1);
 %Neg(1,(L/2)+1:HBand) = Y(1,(L/2)+1:HBand);
 
-N = ifft(Neg);
-figure('Name','Reconstructed Negative Signal');
+N = ifft(Pos);
+figure('Name','Reconstructed Positive Signal');
 plot(Data(:,1), N) 
 title('Reconstructed Negative Signal')
 xlabel('Time (s)')
 ylabel('Pressure change (psi)')
 
-Fin = Data(:,2)-transpose(Neg);
-figure('Name','Processed Signal');
-plot(Data(:,1), Fin);
-title('Processed Signal')
-xlabel('Time (s)')
-ylabel('Pressure change (psi)')
+Y = fft(N);
+L = length(Data);
+Fs = L/(Data(length(Data),1)-Data(1,1));
+
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = Fs*(0:(L/2))/L;
+
+figure('Name','SSPosPowerSpectrum');
+plot(f,P1) 
+title("Single-Sided Amplitude Spectrum of PosX(t)")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
